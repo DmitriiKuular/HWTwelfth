8678c6bd6489f05f35befdae3e995a2ce67739f6
Дмитрий Куулар <kuulardmo@bk.ru>
2022-08-24 19:44:12 +0700

    Added Exceptions massages

--- src/main/java/ru/netology/repository/ProductRepository.java
+++ src/main/java/ru/netology/repository/ProductRepository.java
@@ -1,6 +1,8 @@
 package ru.netology.repository;
 
 import ru.netology.domain.Product;
+import ru.netology.exceptions.AlreadyExistsException;
+import ru.netology.exceptions.NotFoundException;
 
 public class ProductRepository {
     Product[] products = new Product[0];
@@ -9,6 +11,9 @@ public class ProductRepository {
         Product[] tmp = new Product[products.length + 1];
         for (int i = 0; i < products.length; i++) {
             tmp[i] = products[i];
+            if (tmp[i].getId() == product.getId()) {
+                throw new AlreadyExistsException("Товар с таким Id: " + product.getId() + " уже существует");
+            }
         }
         tmp[tmp.length - 1] = product;
         products = tmp;
@@ -18,9 +23,23 @@ public class ProductRepository {
         return products;
     }
 
+    public Product findById(int id) {
+        for (Product product : products) {
+            if (product.getId() == id) {
+                return product;
+            }
+        }
+        return null;
+    }
+
     public Product[] removeById(int id) {
         Product[] tmp = new Product[products.length - 1];
         int copyToIndex = 0;
+
+        if (findById(id) == null) {
+            throw new NotFoundException("Товар с таким Id: " + id + " не найден");
+        }
+
         for (Product product : products) {
             if (product.getId() != id) {
                 tmp[copyToIndex] = product;
--- src/test/java/ru/netology/domain/ProductManagerTest.java
+++ src/test/java/ru/netology/domain/ProductManagerTest.java
@@ -2,6 +2,8 @@ package ru.netology.domain;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import ru.netology.exceptions.AlreadyExistsException;
+import ru.netology.exceptions.NotFoundException;
 import ru.netology.repository.ProductRepository;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,6 +18,8 @@ public class ProductManagerTest {
     Smartphone product4 = new Smartphone(44, "iPhone", 7000, "Apple");
     Smartphone product5 = new Smartphone(55, "i70pl", 88800, "Samsung");
     Smartphone product6 = new Smartphone(66, "RedmiPhone", 33300, "Xiaomi");
+    Book product7 = new Book(33, "Атлант расправил плечи", 5180, "Айн Рэнд");
+    Smartphone product8 = new Smartphone(88, "Телефон", 33300, "Фери");
 
     @BeforeEach
     public void setup() {
@@ -43,8 +47,8 @@ public class ProductManagerTest {
 
     @Test
     public void shouldDeleteProductById() {
-        Product[] expected = {product1, product2, product3, product5, product6};
-        Product[] actual = repo.removeById(44);
+        Product[] expected = {product1, product2, product3, product4, product6};
+        Product[] actual = repo.removeById(55);
         assertArrayEquals(expected, actual);
     }
 
@@ -56,9 +60,28 @@ public class ProductManagerTest {
     }
 
     @Test
-    public void shouldFindBeManufacturer() {
+    public void shouldFindByManufacturer() {
         Product[] expected = {product4, product5};
         Product[] actual = manager.searchBy("pl");
         assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotToDelete() {
+        assertThrows(NotFoundException.class, () -> {repo.removeById(13);});
+    }
+
+    // ДЗ № 13, задача № 2
+    @Test
+    public void shouldAddNewProduct() {
+        repo.save(product8);
+        Product[] expected = {product1, product2, product3, product4, product5, product6,product8};
+        Product[] actual = repo.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotToAddProduct() {
+        assertThrows(AlreadyExistsException.class, () -> {repo.save(product7);});
     }
 }
